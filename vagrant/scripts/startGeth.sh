166675f16dad55987934ec14ca6ada4697e4abb8
crazyquark <sam.bitcoin@gmail.com>
2015-10-09 17:22:49 +0300

    port forwarding for VMs

--- vagrant/scripts/startGeth.sh
+++ vagrant/scripts/startGeth.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+PORT=$1
+
+if [ -z $PORT ]; then
+  PORT=30310
+fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd $DIR
 
-call=`screen /go-ethereum/build/bin/geth --nodiscover --port 30309 --networkid "536354" console`
+call=`screen /go-ethereum/build/bin/geth --nodiscover --port $PORT --networkid "536354" console`
 
 popd

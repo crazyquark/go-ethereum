0e7f7ac26871ddabf78b29674177077ca30bfc77
andreicg <mudpedal@vertexarmy.org>
2015-10-14 11:49:47 +0300

    Fixed useless function

--- run/main.js
+++ run/main.js
@@ -3,5 +3,7 @@ loadScript("utils.js")
 
 //add cristi
 admin.addPeer("enode://90641bbf40d53fdd8a652b56aeffd651c13ddf2f5abec27abe18a3d680d3aca7931059945cb73a438813aec73a44a67bfddc0e22419db7963914604db9fb0e46@10.162.128.122:30309?discport=0")
+
+//add andi
 admin.addPeer("enode://f5b4757b664608e71e930db07e74bc3b5637a3f4a1c3a32d5a4da627be68033967b691ad674a2bdadb0df91e42cd87054dd2cb806f45dc3bee568be954d54e36@10.162.129.79:30309?discport=0")
 miner.start(8)
--- run/utils.js
+++ run/utils.js
@@ -17,39 +17,10 @@ function b() {
 
 function findBlockByTransactionHash(transactionHash, limit) {
 	
-	if (!transactionHash) {
-		console.log ("Transaction hash argument is missing or invalid!");
+	try {
+		return eth.getBlock(eth.getTransaction(transactionHash).blockNumber);
+	} catch (err) {
+		return null;
 	}
-	
-	if (limit === undefined || limit < 1) {
-		limit = 2000;
-	}
-	
-	var blockCursor = eth.getBlock("latest");
-	console.log ("Searching ...");
-	
-	var limitCursor = limit;
-	while (limitCursor > 0) {
-		
-		if (!blockCursor) {
-			console.log ("Reached null block, transaction was not found!");
-			return null;
-		}
-		
-		var it = blockCursor.transactions.indexOf(transactionHash);
-		
-		if (it !== -1) {
-			console.log("Found block");
-			return blockCursor;
-		}
-		
-		
-		var nextBlockNumber = blockCursor.number - 1;
-		blockCursor = eth.getBlock(nextBlockNumber);
-		--limitCursor;
-	}
-	
-	console.log("Reached " + limit + " limit, transaction was not found!");
-	return null;
 	
 }
